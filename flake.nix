3181c7693086d4a22a0b74223902312bb0482bff
Tobias Happ <tobias.happ@gmx.de>
2022-12-13 10:17:01 +0100

    chore: add flake for dev shell

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "original-chattengauer";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/22.11";
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.nodejs_latest
+          pkgs.nodePackages_latest.pnpm
+        ];
+      };
+    };
+}
